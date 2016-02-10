4a9c233597bb03d5185b2ccd0b7c6ae52fd7b0b1
Grigory Baleevskiy <grigory@catalyst-au.net>
2016-02-10 16:49:09 +1100

    template fixes

--- tpl.php
+++ tpl.php
@@ -1,7 +1,20 @@
+<?php
+require_once('../../config.php');
+require('config.php');
+
+$configfieldsdisplay = array_flip(explode(',', get_config('local_directory', 'fields_display')));
+$configfieldsdisplay = array_intersect_key($searchfieldsarray, $configfieldsdisplay);
+if(count($configfieldsdisplay) == 0) {
+    $configfieldsdisplay = $searchfieldsarray;
+}
+
+?>
 <div class="directory">
-    <img data-src="personAvatar" data-alt="name"/>
+    <img data-src="personAvatar" data-alt="firstname"/>
     <div class="directory-container">
+        <?php if (in_array('firstname', $configfieldsdisplay)): ?>
         <div data-content="firstname"></div>
+        <?php endif;?>
         <div data-content="lastname"></div>
         <div class="email" data-content="email"></div>
         <div data-content="skype"></div>
